06c56db9c00513adb4951425e00be4f0b43e3a2a
Daniel Ciborowski <dciborow@microsoft.com>
2022-09-28 01:36:32 +0000

    deleted:    samples/managed-application/base-image-vm/README.md 	deleted:    samples/managed-application/base-image-vm/app-contents/WriteText.ps1 	deleted:    samples/managed-application/base-image-vm/app-contents/createUiDefinition.json 	deleted:    samples/managed-application/base-image-vm/app-contents/mainTemplate.bicep 	deleted:    samples/managed-application/base-image-vm/app-contents/parameters.json.tmpl 	deleted:    samples/managed-application/base-image-vm/app-contents/viewDefinition.json 	deleted:    samples/managed-application/base-image-vm/app-contents/writeTextExtension.bicep 	deleted:    samples/managed-application/base-image-vm/listing_config.json 	deleted:    samples/managed-application/base-image-vm/manifest.yml 	deleted:    samples/managed-application/base-image-vm/r_216_216.png 	deleted:    samples/managed-application/base-image-vm/r_255_115.png 	deleted:    samples/managed-application/base-image-vm/r_48_48.png 	deleted:    samples/managed-application/base-image-vm/r_90_90.png 	deleted:    samples/solution-template/base-image-vm/README.md 	deleted:    samples/solution-template/base-image-vm/app-contents/WriteText.ps1 	deleted:    samples/solution-template/base-image-vm/app-contents/createUiDefinition.json 	deleted:    samples/solution-template/base-image-vm/app-contents/mainTemplate.bicep 	deleted:    samples/solution-template/base-image-vm/app-contents/parameters.json.tmpl 	deleted:    samples/solution-template/base-image-vm/app-contents/writeTextExtension.bicep 	deleted:    samples/solution-template/base-image-vm/listing_config.json 	deleted:    samples/solution-template/base-image-vm/manifest.yml 	deleted:    samples/solution-template/base-image-vm/r_216_216.png 	deleted:    samples/solution-template/base-image-vm/r_255_115.png 	deleted:    samples/solution-template/base-image-vm/r_48_48.png 	deleted:    samples/solution-template/base-image-vm/r_90_90.png 	deleted:    samples/virtual-machine/basic-windows-vm/README.md 	deleted:    samples/virtual-machine/basic-windows-vm/build.BasicWindowsVMImage.pkr.hcl 	deleted:    samples/virtual-machine/basic-windows-vm/logos/large.png 	deleted:    samples/virtual-machine/basic-windows-vm/logos/medium.png 	deleted:    samples/virtual-machine/basic-windows-vm/logos/small.png 	deleted:    samples/virtual-machine/basic-windows-vm/logos/wide.png 	deleted:    samples/virtual-machine/basic-windows-vm/source.windowsvhd.pkr.hcl 	deleted:    samples/virtual-machine/basic-windows-vm/variables.pkr.json 	deleted:    samples/virtual-machine/basic-windows-vm/vmOfferConfig.json

--- samples/solution-template/base-image-vm/app-contents/WriteText.ps1
+++
@@ -1,9 +0,0 @@
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# Licensed under the MIT License.
-Param (
-    [String] $fileName = "MyFile",
-    [String] $fileContent = "MyContent"
-)
-
-Set-Location -Path "C:\"
-Set-Content "$fileName.txt" $fileContent
